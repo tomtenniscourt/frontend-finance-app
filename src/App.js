50f715b0f785653419475e79a8e6724f8326c710
Tom_Court <tomcourt94@icloud.com>
2023-10-10 15:21:52 +0100

    Tom test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 
 function App() {
   console.log('Jethro Test')
+  console.log('Tom test')
   return (
     <div className="App">
      <p>hello world</p>
-    </div>
+         </div>
   );
 }
 
