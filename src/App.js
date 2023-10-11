269f5f757e51492ba3f890f83c32f31404ca7c71
Jethro_Block <jethroblock22@gmail.COM>
2023-10-11 14:29:18 +0100

    add temporary map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
 import { getAllUsers } from './APIs/UserAPIs';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+const [users, setUsers] = useState([])
+
   useEffect(() => {
     getAllUsers()
-      .then(data => console.log('User data: ', data))
+      .then(data => {
+        console.log('User data: ', data);
+         setUsers(data)}
+         )
       .catch(error => console.log('getAllUsers error: ', error))
   }, [])
   
   return (
     <div className="App">
      <p>hello world</p>
+{users.map((user) => (
+  <div key={user.id} style={{display: "inline-flex", margin: "10px"}}> 
+  <p >{user.name}</p>
+  <p>{user.email}</p>
+</div>
+))}
+
     </div>
   );
 }
