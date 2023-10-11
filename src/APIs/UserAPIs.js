dbd6df77b27b3320351dfefce2a9888241482bed
Catherinen29 <cat_nevin@hotmail.com>
2023-10-11 10:40:53 +0100

    Update UserAPIs file.

--- src/APIs/UserAPIs.js
+++ src/APIs/UserAPIs.js
@@ -2,6 +2,7 @@ import axiosInstance from "./AxiosInstance"
 
 export const getAllUsers = async () => {
     const response = await axiosInstance.get("/users");
+    console.log(response)
     return response.data;
 }
 
