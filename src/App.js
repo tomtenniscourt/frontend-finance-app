bba00bc29336d7f04a3b9cf5ffda5277ff03b028
Jethro_Block <jethroblock22@gmail.COM>
2023-10-11 15:24:42 +0100

    add create user form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import { getAllUsers } from './APIs/UserAPIs';
+import { createUser, getAllUsers } from './APIs/UserAPIs';
 import { useEffect, useState } from 'react';
 
 function App() {
 
 const [users, setUsers] = useState([])
+const [formInput, setFormInput] = useState({
+  name: "",
+  email: ""
+})
 
   useEffect(() => {
     getAllUsers()
@@ -15,6 +19,29 @@ const [users, setUsers] = useState([])
       .catch(error => console.log('getAllUsers error: ', error))
   }, [])
   
+
+  const handleChange = (e) => {
+    setFormInput({
+      ...formInput,
+      [e.target.name]: e.target.value
+    })
+  }
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+  try {
+    await createUser(formInput)
+  } catch (err) {
+    console.error(err)
+  }
+  finally {
+    setFormInput({
+      name: "",
+      email: ""
+    })
+  }
+}
+
   return (
     <div className="App">
      <p>hello world</p>
@@ -24,6 +51,26 @@ const [users, setUsers] = useState([])
   <p>{user.email}</p>
 </div>
 ))}
+
+<form onSubmit={handleSubmit}>
+<input
+type="text"
+label="name"
+value={formInput.name}
+name="name"
+onChange={handleChange}
+>
+</input>
+
+<input
+type="text"
+label="email"
+value={formInput.email}
+name="email"
+onChange={handleChange}
+>
+</input>
+</form>
 
     </div>
   );
