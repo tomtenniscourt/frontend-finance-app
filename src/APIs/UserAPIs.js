9f4dc1981571364cbc94c5344473aeb05907d3ab
Jethro_Block <jethroblock22@gmail.COM>
2023-10-12 10:21:07 +0100

    env check console log

--- src/APIs/UserAPIs.js
+++ src/APIs/UserAPIs.js
@@ -7,6 +7,7 @@ export const getAllUsers = async () => {
 }
 
 export const createUser = async (userDetails) => {
+console.log('process.env.NODE_ENV',process.env.NODE_ENV)
 const response = await axiosInstance.post("/users", userDetails)
 console.log('createUserResponse', response)
 return response.data
