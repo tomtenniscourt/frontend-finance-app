3e160588283d497bd82c09793f05278a1211e47d
Catherinen29 <cat_nevin@hotmail.com>
2023-10-13 15:11:49 +0100

    Add password field to form and include in formInput state.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { createUser, getAllUsers } from './APIs/UserAPIs';
+import { createUser, deleteOneUser, getAllUsers, getOneUser, updateOneUser } from './APIs/UserAPIs';
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -8,9 +8,22 @@ const [users, setUsers] = useState([])
 const [formInput, setFormInput] = useState({
   firstname: "",
   lastname: "",
-  email: ""
+  email: "",
+  password: ""
 })
 console.log('formInput', formInput)
+
+const handleGetOneUser = () => {
+  getOneUser("652676a7db284fe975366ea0")
+.then((data) => console.log(data))
+.catch((err) => console.log(err))
+}
+
+const handleDeleteUser = () => {
+  deleteOneUser("652676a7db284fe975366ea0")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+}
 
   useEffect(() => {
     getAllUsers()
@@ -37,12 +50,21 @@ const handleSubmit = async (e) => {
   } catch (err) {
     console.error(err)
   }
-  // finally {
-  //   setFormInput({
-  //     name: "",
-  //     email: ""
-  //   })
-  // }
+  finally {
+    setFormInput({
+      name: "",
+      email: ""
+    })
+  }
+}
+
+const handleUpdateSubmit = async (e) => {
+  e.preventDefault()
+  try {
+    await updateOneUser("652676a7db284fe975366ea0",formInput)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
   return (
@@ -53,11 +75,12 @@ const handleSubmit = async (e) => {
   <p>{user.firstname}</p>
   <p>{user.lastname}</p>
   <p>{user.email}</p>
+  <p>{user._id}</p>
 </div>
 ))}
 
 <form onSubmit={handleSubmit}>
-
+<h2> Creating a user </h2>
 <input
 type="text"
 label="firstname"
@@ -91,8 +114,39 @@ placeholder="email"
 >
 </input>
 
+<input
+type="text"
+label="password"
+value={formInput.password}
+name="password"
+onChange={handleChange}
+placeholder="password"
+
+>
+</input>
+
 <button type='submit'>Submit</button>
 </form>
+
+<h2>Getting One User</h2>
+<button onClick={handleGetOneUser}>Get One User!</button>
+
+<h2>Updating one user</h2>
+<form onSubmit={handleUpdateSubmit}>
+<input
+type="text"
+label="firstname"
+value={formInput.firstname}
+name="firstname"
+onChange={handleChange}
+placeholder="firstname"
+></input>
+<button type='submit'>Update User</button>
+</form>
+
+
+<h2>Deleting One User</h2>
+<button onClick={handleDeleteUser}>Delete One User</button>
 
     </div>
   );
