b7b82b80dab0af5999726e95075866c319cd59cc
Catherinen29 <cat_nevin@hotmail.com>
2023-11-02 15:43:14 +0000

    Build out CheckAccessExpiry component To get current user and regenerateAccessToken.

--- src/Auth/CheckAccessExpiry.js
+++ src/Auth/CheckAccessExpiry.js
@@ -1,25 +1,37 @@
+import { regenerateAccessToken, getCurrentUser } from "../APIs/UserAPIs"
+import { useState } from "react" 
 
 
-const checkAccessTokenExpiry = async () => {
-
+const CheckAccessTokenExpiry = ({userId}) => {
+    console.log('***CHECK CALLED ****')
     const token = window.localStorage.getItem("Token")
 
     const tokenExpirationTime = window.localStorage.getItem("ExpiresAt")
+
+    const [userEmail, setUserEmail] = useState("")
 
     try {
         const currentTimestamp = Math.floor(Date.now() / 1000)
 
         // keep this check, but it'll be in a slightly different format (no payload variable)
         // we only want to refresh the token if it's going to expire very soon (seconds away)
-if (currentTimestamp < tokenExpirationTime - 5000) {
+    if (currentTimestamp < tokenExpirationTime - 10) {
     // API call to regenerate Access Token.
 
     // User email required.
-        } else {
-            return;
+        console.log('Token all good')
+        } 
+        else {
+        console.log('Token about to expire')
+        getCurrentUser(userId)
+            .then(data => setUserEmail(data.email))
+            .catch(error => console.log('Get current user error: ', error))
+        userEmail && regenerateAccessToken(userEmail)
         }
     }
     catch (err) {
         console.log('Error verifying access token:', err);
     }
 }
+
+export default CheckAccessTokenExpiry;
